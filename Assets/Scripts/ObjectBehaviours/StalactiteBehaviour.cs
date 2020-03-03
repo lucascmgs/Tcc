cf4f7f5454f096b3535de082400c3fbe12e48d90
lucascmb <lucas.cmb03@gmail.com>
2020-03-03 00:25:36 -0300

    limites impostos na movimentação do submarino, colisao da estalactite com o chão, sprites adicionadas e randomizadas

--- Assets/Scripts/ObjectBehaviours/StalactiteBehaviour.cs
+++ Assets/Scripts/ObjectBehaviours/StalactiteBehaviour.cs
@@ -11,15 +11,17 @@ public class StalactiteBehaviour : MonoBehaviour
     private Rigidbody2D rb;
     
     bool verify;
+    bool alreadyFallen;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         verify = true;
+        alreadyFallen = false;
     }
     void Update()
     {
-        if (verify)
+        if (verify && !alreadyFallen)
         {
             checkIfFalling();
             timeElapsed = Time.time;
@@ -38,12 +40,14 @@ public class StalactiteBehaviour : MonoBehaviour
         if(UnityEngine.Random.value > randomValueToFall)
         {
             rb.velocity = new Vector2(rb.velocity.x, -speed);
+            alreadyFallen = true;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Bullet") Destroy(this.gameObject);
+        else if (collision.tag == "Floor") this.rb.velocity = new Vector2(rb.velocity.x, 0);
     }
 
     private void OnBecameInvisible()
--- Assets/Scripts/SubMovement.cs
+++ Assets/Scripts/SubMovement.cs
@@ -13,23 +13,60 @@ public class SubMovement : MonoBehaviour
     private float horizontalSpeed = 5f;
     [SerializeField]
     private float verticalSpeed = 3f;
-    
+    [SerializeField]
+    GameObject floorPrefab;
+    [SerializeField]
+    GameObject ceilingPrefab;
+
+    int resolutionWidth;
+    int resolutionHeight;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        resolutionWidth = Screen.currentResolution.width;
+        resolutionHeight = Screen.currentResolution.height;
     }
 
     public void Update()
     {
+        checkMovement();
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         var movement = context.ReadValue<Vector2>();
+        var limits = Camera.main.WorldToViewportPoint(this.transform.position);
+
         movement.x *= horizontalSpeed;
         movement.y *= verticalSpeed;
 
         rb.velocity = movement;
     }
-    
+
+    void checkMovement()
+    {
+        
+        if (this.transform.position.y + ceilingPrefab.GetComponent<SpriteRenderer>().size.y / 2 >= Camera.main.orthographicSize)
+        {
+            this.transform.position = new Vector2(this.transform.position.x, Camera.main.orthographicSize - ceilingPrefab.GetComponent<SpriteRenderer>().size.y / 2);
+        }
+
+        else if(this.transform.position.y - floorPrefab.GetComponent<SpriteRenderer>().size.y <= -Camera.main.orthographicSize)
+        {
+            this.transform.position = new Vector2(this.transform.position.x, -Camera.main.orthographicSize + floorPrefab.GetComponent<SpriteRenderer>().size.y);
+        }
+
+        if (this.transform.position.x + this.GetComponent<SpriteRenderer>().size.x >= ( (Camera.main.orthographicSize * resolutionWidth) / resolutionHeight ))
+        {
+            this.transform.position = new Vector2(((Camera.main.orthographicSize * resolutionWidth) / resolutionHeight ) - this.GetComponent<SpriteRenderer>().size.x, this.transform.position.y);
+        }
+
+        else if (this.transform.position.x - this.GetComponent<SpriteRenderer>().size.x <= ( -(Camera.main.orthographicSize * resolutionWidth) / resolutionHeight))
+        {
+            this.transform.position = new Vector2(-((Camera.main.orthographicSize * resolutionWidth) / resolutionHeight) + this.GetComponent<SpriteRenderer>().size.x, this.transform.position.y);
+        }
+
+    }
+
 }
