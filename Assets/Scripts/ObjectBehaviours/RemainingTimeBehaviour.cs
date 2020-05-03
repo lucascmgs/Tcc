234618aece6629d5da335690cd7df7290d826093
Lucas <lucascmgsds@gmail.com>
2020-05-03 18:30:10 -0300

    UI de tempo para o submarino

---
+++ Assets/Scripts/ObjectBehaviours/RemainingTimeBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RemainingTimeBehaviour : MonoBehaviour
+{
+    public float initialRemainingTime = 120;
+    private TextMeshProUGUI timeText;
+    
+    [NonSerialized] public float remainingTime;
+    void Start()
+    {
+        remainingTime = initialRemainingTime;
+        timeText = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        if ((int) remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        timeText.text = "" + (int) remainingTime;
+    }
+}
---
+++ Assets/Scripts/UI/UITImeFrame.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITImeFrame : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> sprites;
+
+    private RemainingTimeBehaviour _timeBehaviour;
+
+    private float initialTime;
+
+    private Image _image;
+
+    void Start()
+    {
+        _timeBehaviour = FindObjectOfType<RemainingTimeBehaviour>();
+        _image = GetComponent<Image>();
+        initialTime = _timeBehaviour.initialRemainingTime;
+    }
+
+
+    void Update()
+    {
+        float remainingTimeRatio = _timeBehaviour.remainingTime / initialTime;
+
+        int imageIndex = 0;
+
+        if (remainingTimeRatio > 0.75)
+        {
+            imageIndex = 0;
+        }
+        else if (remainingTimeRatio > 0.5 && remainingTimeRatio <= 0.75)
+        {
+            imageIndex = 1;
+        }
+        else if (remainingTimeRatio > 0.25 && remainingTimeRatio <= 0.5)
+        {
+            imageIndex = 2;
+        }
+        else if (remainingTimeRatio > 0 && remainingTimeRatio <= 0.25)
+        {
+            imageIndex = 3;
+        }
+        else
+        {
+            imageIndex = 4;
+        }
+
+        _image.sprite = sprites[imageIndex];
+    }
+}
