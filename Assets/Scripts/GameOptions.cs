0e4dac76ef5138ce775b9dbcfd469fd6c2ac42a0
Lucas <lucascmgsds@gmail.com>
2020-05-21 22:28:00 -0300

    Adicionado PlayerPrefs para o tempo de jogo

--- Assets/Scripts/GameOptions.cs
+++ Assets/Scripts/GameOptions.cs
@@ -1,9 +1,22 @@
-﻿namespace DefaultNamespace
+﻿using UnityEngine;
+
+namespace DefaultNamespace
 {
     public static class GameOptions
     {
         public static Gamestate gameState = Gamestate.NotStarted;
-        public static float gameTime = 120;
+        
+        public static float GameTime
+        {
+            get
+            {
+                return PlayerPrefs.GetFloat("GameTime", 100);
+            } set
+            {
+                PlayerPrefs.SetFloat("GameTime", value);
+            }
+        }
+
         public static bool playMusic = true;
         public static bool playSounds = true;
     }
